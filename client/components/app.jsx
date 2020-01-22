a101f88a176da27e1110bb2b40888c2d0d79edcf
david <dholic9@github.com>
2020-01-22 20:01:20 +0000

    testing

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppContext from '../lib/context';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -18,8 +19,8 @@ export default class App extends React.Component {
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message}</h1>;
+    return (
+
+    )
   }
 }
