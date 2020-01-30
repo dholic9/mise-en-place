ad9129ab472123a3a0b10d660f700b2b1d0c0e0a
david <dholic9@github.com>
2020-01-30 01:09:29 +0000

    styling

--- client/components/top-bar.jsx
+++ client/components/top-bar.jsx
@@ -22,7 +22,6 @@ export default class TopBar extends React.Component {
         justify-content-center
         d-flex
         row
-        border-bottom
         align-items-center
         "
       >
